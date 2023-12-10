d3ad93b821c7a9c6871b3dc27aeaec4f91af9412
m_goldbach <m_goldbach@utb.cz>
2023-12-10 13:17:51 +0100

    Lazy Column with articles

--- app/src/main/java/com/example/newsapp/presentation/news_screen/NewsScreen.kt
+++ app/src/main/java/com/example/newsapp/presentation/news_screen/NewsScreen.kt
@@ -6,17 +6,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,6 +32,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.newsapp.Greeting
 import com.example.newsapp.R
 
 @Composable
@@ -37,11 +43,43 @@ fun NewsScreen(
 }
 
 @Composable
+fun NewsList(
+    modifier : Modifier = Modifier,
+    ){
+    LazyColumn(
+        modifier = modifier
+            .padding(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+
+    ){
+        items(10){
+            NewsArticleCard()
+        }
+
+    }
+
+
+}
+
+@Composable
+@Preview
+fun NewsListPreview(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+       NewsList()
+    }
+
+}
+
+@Composable
 fun NewsArticleCard(
     modifier : Modifier = Modifier,
     ){
     Card(
-        modifier = modifier.clickable {  }
+        elevation = CardDefaults.cardElevation(4.dp),
+        modifier = modifier.clickable {  },
     ){
         Row(
             modifier = Modifier
@@ -57,7 +95,8 @@ fun NewsArticleCard(
             ) {
                 Text(
                     text = "CATEGORY",
-                    style = MaterialTheme.typography.titleSmall,
+                    style = MaterialTheme.typography.titleSmall.copy(
+                        color = MaterialTheme.colorScheme.primary),
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis
                 )
@@ -77,12 +116,22 @@ fun NewsArticleCard(
                 ) {
                     Text(
                         text = "Source",
-                        style = MaterialTheme.typography.bodySmall,
+                        style = MaterialTheme.typography.bodySmall.copy(
+                            color = MaterialTheme.colorScheme.secondary)
+                    )
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    //add dot
+
+                    Text(
+                        text = "•",
+                        style = MaterialTheme.typography.bodySmall.copy(
+                            color = MaterialTheme.colorScheme.secondary),
                     )
                     Spacer(modifier = Modifier.padding(8.dp))
                     Text(
                         text = "Date",
-                        style = MaterialTheme.typography.bodySmall,
+                        style = MaterialTheme.typography.bodySmall.copy(
+                            color = MaterialTheme.colorScheme.secondary),
                     )
 
                 }
