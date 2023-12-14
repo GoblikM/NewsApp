ca6eb8cef5ba683dbd76c8d96dc297bc691dd4ed
m_goldbach <m_goldbach@utb.cz>
2023-12-14 15:34:55 +0100

    Scroll to top floating action button

--- app/src/main/java/com/example/newsapp/presentation/news_screen/NewsScreen.kt
+++ app/src/main/java/com/example/newsapp/presentation/news_screen/NewsScreen.kt
@@ -6,29 +6,40 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.newsapp.domain.model.newsResponse.Result
 import com.example.newsapp.presentation.news_screen.components.NewsArticleCard
 import com.example.newsapp.presentation.shared_components.TopBar
 import com.example.newsapp.ui.theme.NewsAppTheme
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewsScreen(
     state: NewsScreenState,
-    onCardClicked: (Result) -> Unit
-)
-{
+    onCardClicked: (Result) -> Unit,
+) {
+    val lazyListState = rememberLazyListState()
+    val scope = rememberCoroutineScope()
+
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
@@ -39,6 +50,18 @@ fun NewsScreen(
                 onSearchIconClicked = {}
             )
         },
+        floatingActionButton = {
+            // Use the LazyListState to scroll to the top
+            val density = LocalDensity.current.density
+            FloatingActionButton(onClick = {scope.launch {
+                lazyListState.animateScrollToItem(0) }
+            }) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowUp,
+                    contentDescription = "Scroll to top"
+                )
+            }
+        },
         content = { padding ->
             Column(
                 modifier = Modifier
@@ -48,6 +71,7 @@ fun NewsScreen(
                 NewsList(
                     state = state,
                     onCardClicked = onCardClicked,
+                    lazyListState = lazyListState,
                 )
 
             }
@@ -59,15 +83,16 @@ fun NewsScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview(showBackground = true)
-fun NewsScreenPreview(){
+fun NewsScreenPreview() {
     NewsAppTheme {
         NewsScreen(state = NewsScreenState(), onCardClicked = {})
     }
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
-fun NewsScreenPreviewNightMode(){
+fun NewsScreenPreviewNightMode() {
     NewsAppTheme {
         NewsScreen(state = NewsScreenState(), onCardClicked = {})
     }
@@ -77,18 +102,20 @@ fun NewsScreenPreviewNightMode(){
 fun NewsList(
     state: NewsScreenState,
     onCardClicked: (Result) -> Unit,
-    modifier : Modifier = Modifier,
-    ){
+    modifier: Modifier = Modifier,
+    lazyListState: LazyListState
+) {
     Surface(
         color = MaterialTheme.colorScheme.background
     ) {
         LazyColumn(
+            state = lazyListState,
             modifier = modifier
                 .padding(12.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp)
 
-        ){
-            items(state.articles){ article ->
+        ) {
+            items(state.articles) { article ->
 
                 NewsArticleCard(
                     article = article,
@@ -99,6 +126,9 @@ fun NewsList(
         }
     }
 }
+
+
+
 
 
 
