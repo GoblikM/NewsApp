43459d54b2a6a8d7e580dee0d3f902741a0678a4
m_goldbach <m_goldbach@utb.cz>
2023-12-15 11:22:42 +0100

    SavedArticlesScreen, empty list of saved articles message and animation

--- app/src/main/java/com/example/newsapp/presentation/saved_articles_screen/SavedArticlesScreen.kt
+++ app/src/main/java/com/example/newsapp/presentation/saved_articles_screen/SavedArticlesScreen.kt
@@ -1,12 +1,16 @@
 package com.example.newsapp.presentation.saved_articles_screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -16,9 +20,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.newsapp.R
 import com.example.newsapp.domain.model.newsResponse.Result
 import com.example.newsapp.presentation.news_screen.components.NewsArticleCard
@@ -30,7 +44,7 @@ fun SavedArticlesScreen(
     onCardClicked: (Result) -> Unit,
     onReturnBntClicked: () -> Unit,
 
-) {
+    ) {
     val scrollState = rememberScrollState()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
@@ -38,12 +52,14 @@ fun SavedArticlesScreen(
         .nestedScroll(scrollBehavior.nestedScrollConnection)
         .fillMaxSize(),
         topBar = {
-            TopAppBar(scrollBehavior = scrollBehavior,
+            TopAppBar(
+                scrollBehavior = scrollBehavior,
                 title = {
                     Text(
-                    text = stringResource(id = R.string.saved_news),
-                    style = MaterialTheme.typography.titleMedium,
-                )},
+                        text = stringResource(id = R.string.saved_news),
+                        style = MaterialTheme.typography.titleMedium,
+                    )
+                },
                 actions = {
                     IconButton(onClick = { onReturnBntClicked() }) {
                         Icon(
@@ -81,6 +97,8 @@ fun SavedArticlesList(
     state: SavedArticlesScreenState,
     onCardClicked: (Result) -> Unit
 ) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.no_saved_articles))
+
     LazyColumn(modifier = modifier) {
         items(state.articles) { article ->
             NewsArticleCard(
@@ -89,4 +107,48 @@ fun SavedArticlesList(
             )
         }
     }
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+
+    ) {
+        Column {
+            if (state.isLoading) {
+                CircularProgressIndicator()
+            }
+            if (state.articles.isEmpty() && !state.isLoading) {
+                LottieAnimation(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f, fill = true),
+                    composition = composition,
+                    iterations = LottieConstants.IterateForever,
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .weight(1f),
+                    text = "Nemáte uložené žádné zprávy!",
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        color = MaterialTheme.colorScheme.onBackground
+                    ),
+                    textAlign = TextAlign.Center
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+@Preview
+fun SavedArticlesScreenPreview() {
+    SavedArticlesScreen(
+        state = SavedArticlesScreenState(),
+        onCardClicked = {},
+        onReturnBntClicked = {}
+    )
 }
